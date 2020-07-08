6326706f590416b89685e4bb7d617f70df2d2960
hanchaobiao <1017421922@qq.com>
2020-07-08 15:59:48 +0800

    1、修改税率填写验证

--- apps/admin_operation/forms.py
+++ apps/admin_operation/forms.py
@@ -83,7 +83,7 @@ class TaxProgressForm(Form):
     """
     complete_tax_money = DecimalField("完成税额",  places=10, rounding=2)
     total_tax_money = DecimalField("模板税额", places=10, rounding=2)
-    year = StringField("年份", validators=[DataRequired(message="年份不能为空"), Length(min=4, max=4)])
+    year = IntegerField("年份", validators=[NumberRange(min=1900, max=2100)])
 
 
 class AddDepartmentNoticeForm(Form):
